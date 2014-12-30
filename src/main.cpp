1f1ea44c26f4bcc3d064aefbe4a7663261a9b22b
helori <helori.lanos@gmail.com>
2014-12-30 18:36:57 +0100

    Version 0.0.8

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
     
     a.setApplicationName("Chronomodel");
     a.setApplicationDisplayName("Chronomodel");
-    a.setApplicationVersion("0.0.7");
+    a.setApplicationVersion("0.0.8");
     a.setOrganizationDomain("http://www.chronomodel.com");
     a.setOrganizationName("CNRS");
     a.setWindowIcon(QIcon(":chronomodel.png"));
