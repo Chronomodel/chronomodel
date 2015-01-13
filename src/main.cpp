99dc349eef334fb084f610a4ce60a7321cab4298
helori <helori.lanos@gmail.com>
2015-01-13 11:26:16 +0100

    Localisation réglée sur Etats-Unis, anglais (pour le format des nombres).

--- src/main.cpp
+++ src/main.cpp
@@ -80,6 +80,8 @@ int main(int argc, char *argv[])
         }
     }
     
+    QLocale::setDefault(QLocale(QLocale::English, QLocale::UnitedStates));
+    
     //if(argv)
     //QString filePath(argv[1]);
     
