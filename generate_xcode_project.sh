93a9c06877534a125cc2d21af5dd972198866022
chrono35 <philippe.dufresne35@gmail.com>
2015-06-22 17:33:05 +0200

    Script to create the XCode project in debug mode modified : path to Qt must be given as argument

--- generate_xcode_project.sh
+++ generate_xcode_project.sh
@@ -2,7 +2,7 @@
 
 ROOT_PATH=$(dirname $0)
 BUNDLE_PATH=Release/Chronomodel.app
-QT_BIN_PATH=/Users/helori/Qt5.2.1/5.2.1/clang_64/bin
+QT_BIN_PATH=$1
 
 cd $ROOT_PATH
 ${QT_BIN_PATH}/qmake -spec macx-xcode "CONFIG+=debug" $ROOT_PATH/Chronomodel.pro
