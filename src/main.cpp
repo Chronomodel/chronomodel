70ee3e58741fc8f70e43a2b6f461ef62afb59699
Chrono35 <philippe.dufresne35@gmail.com>
2015-06-29 15:55:40 +0200

    Version 1.2.11
    Signed-off-by: Chrono35 <philippe.dufresne35@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ int main(int argc, char *argv[])
     
     a.setApplicationName("ChronoModel");
     a.setApplicationDisplayName("ChronoModel");
-    a.setApplicationVersion("1.2.10");
+    a.setApplicationVersion("1.2.11");
     a.setOrganizationDomain("http://www.chronomodel.com");
     a.setOrganizationName("CNRS");
     a.setWindowIcon(QIcon(":chronomodel.png"));
