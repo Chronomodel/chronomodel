93f478cd0e2513912f9b23fbaa35031f0cc6147f
chrono35 <philippe.dufresne35@gmail.com>
2016-01-15 14:35:36 +0100

    Change display in case of impossible Calibration. Now the text "Individual calibration not digitally computable" is set on the last line. it come when a date.isValid=false

--- src/ui/panel_model/data/DatesListItemDelegate.h
+++ src/ui/panel_model/data/DatesListItemDelegate.h
@@ -20,11 +20,6 @@ public:
         font.setPointSizeF(pointSize(11));
         QFontMetrics metrics(font);
         
-        /*int mm = 2;
-        int mh = metrics.height();
-        int butH = 20;
-        return QSize(option.rect.width(), 4*mh + butH + 6*mm);*/
-        
         int mm = 2;
         int mh = metrics.height();
         return QSize(option.rect.width(), 4*mh + 5*mm);
@@ -39,20 +34,16 @@ public:
         int h = option.rect.height();
         int iconW = 30;
         int iconS = 20;
-        //int butH = 20;
-        //int butW = 60;
         
         painter->setRenderHint(QPainter::Antialiasing);
         
-        if(option.state & QStyle::State_Selected)
-        {
-            //painter->fillRect(option.rect, option.palette.highlight());
+        if(option.state & QStyle::State_Selected) {
             painter->fillRect(option.rect, QColor(230, 230, 230));
         }
         QString dateName = index.model()->data(index, 0x0101).toString();
         QString dateDesc = index.model()->data(index, 0x0103).toString();
         QString pluginId = index.model()->data(index, 0x0102).toString();
-        QString delta = index.model()->data(index, 0x0105).toString();
+        QString deltaText = index.model()->data(index, 0x0105).toString();
         QString dateMethodStr = index.model()->data(index, 0x0106).toString();
         bool isValid = index.model()->data(index, 0x0107).toBool();
         bool isCombined = index.model()->data(index, 0x0108).toBool();
@@ -71,13 +62,14 @@ public:
             QFont font = option.font;
             font.setPointSizeF(pointSize(16));
             painter->setFont(font);
+             /* this part draw a white cross in a red circle
             if(!isValid){
                 painter->setBrush(Qt::red);
                 painter->setPen(Qt::red);
                 painter->drawEllipse(ix, iy, is, is);
                 painter->setPen(Qt::white);
                 painter->drawText(ix, iy, is, is, Qt::AlignCenter, "X");
-            }
+            }*/
             if(isCombined){
                 painter->setBrush(Painting::mainColorDark);
                 painter->setPen(Painting::mainColorDark);
@@ -104,10 +96,16 @@ public:
             painter->setPen(QColor(120, 120, 120));
             painter->drawText(x + iconW, y + 2*mm + mh, w - iconW, mh, Qt::AlignLeft | Qt::AlignVCenter, tr("Type") + " : " + plugin->getName() + " | " + tr("Method") + " : " + dateMethodStr);
             painter->drawText(x + iconW, y + 3*mm + 2*mh, w - iconW, mh, Qt::AlignLeft | Qt::AlignVCenter, dateDesc);
-            
+
             painter->setPen(Painting::mainColorLight);
-            painter->drawText(x + iconW, y + 4*mm + 3*mh, w - iconW, mh, Qt::AlignLeft | Qt::AlignVCenter, delta);
-            
+            painter->drawText(x + iconW, y + 4*mm + 3*mh, w - iconW, mh, Qt::AlignLeft | Qt::AlignVCenter, deltaText);
+
+
+            if(!isValid){
+                painter->setPen(Qt::black);
+                QString str = tr("Individual calibration not digitally computable");
+                painter->drawText(x + iconW, y + 4*mm + 3*mh, w - iconW, mh, Qt::AlignRight | Qt::AlignVCenter, str );
+            }
             painter->setPen(QColor(200, 200, 200));
             painter->drawLine(x, y + h, x + w, y + h);
             
