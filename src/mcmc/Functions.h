d03e9c73ab0ea440739bdbdf0ec5f07257cd78cb
chrono35 <philippe.dufresne35@gmail.com>
2016-07-19 17:04:05 +0200

    Add function transitionRangeFromTraces() which transfert to timeRangeFromTraces

--- src/mcmc/Functions.h
+++ src/mcmc/Functions.h
@@ -39,22 +39,21 @@ Quartiles quartilesForTrace(const QVector<double>& trace);
 Quartiles quartilesForRepartition(const QVector<double>& repartition,const double tmin,const double step);
 QPair<double, double> credibilityForTrace(const QVector<double>& trace, double thresh, double& exactThresholdResult, const QString description = "Credibility computation");
 QPair<double, double> timeRangeFromTraces(const QVector<double>& trace1, const QVector<double>& trace2, const double thresh, const QString description ="Time Range Computation");
+
 QPair<double, double> gapRangeFromTraces(const QVector<double>& trace1, const QVector<double>& trace2, const double thresh, const QString description ="Gap Range Computation");
+QPair<double, double> transitionRangeFromTraces(const QVector<double>& trace1, const QVector<double>& trace2, const double thresh, const QString description ="Gap Range Computation");
 
 QString intervalText(const QPair<double, QPair<double, double> >& interval, FormatFunc formatFunc = 0);
 QString getHPDText(const QMap<double, double>& hpd, double thresh, const QString& unit = QString(), FormatFunc formatFunc = 0);
 QList<QPair<double, QPair<double, double> > > intervalsForHpd(const QMap<double, double>& hpd, double thresh);
 
-inline double rounddouble(double f, int prec)
+inline double rounddouble(const double f,const int prec)
 {
     double result;
-    if(prec > 0)
-    {
-        double factor = pow(10.f, (double)prec);
+    if (prec > 0){
+        const double factor = pow(10.f, (double)prec);
         result = round(f * factor) / factor;
-    }
-    else
-    {
+    } else {
         result = round(f);
     }
     return result;
