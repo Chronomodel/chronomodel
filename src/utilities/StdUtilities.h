62dc9d01bff3d3efa581c4df3fbcaa717c4729e2
Dufresne Philippe <philippe.dufresne35@gmail.com>
2016-09-12 17:21:42 +0200

    enhancement of the interpolate function
    Now we can have two kinds of type

--- src/utilities/StdUtilities.h
+++ src/utilities/StdUtilities.h
@@ -34,10 +34,10 @@ double safeLog(const double& x, int n = 5);
 
 void checkFloatingPointException(const QString& infos = QString());
 
-template <typename T>
-T interpolate(const T& x, const T& x1, const T& x2, const T& y1, const T& y2)
+template <typename T, typename V>
+V interpolate(const T& x, const T& x1, const T& x2, const V& y1, const V& y2)
 {
-    return (y1 + (y2 - y1) * (x - x1) / (x2 - x1));
+    return (y1 + (y2 - y1) * (V)((x - x1) / (x2 - x1)) );
 }
 
 template <typename T, typename U>
