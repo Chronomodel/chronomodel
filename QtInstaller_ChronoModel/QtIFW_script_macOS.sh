1f455a5eadd8994b5a8bdb2df6182973fd81fceb
Dufresne <philippe.dufresne35@gmail.com>
2024-02-05 17:04:54 +0100

    update installer

--- QtInstaller_ChronoModel/QtIFW_script_macOS.sh
+++ QtInstaller_ChronoModel/QtIFW_script_macOS.sh
@@ -2,12 +2,12 @@
 # ne pas mettre de blanc autour de =
 # penser à faire Ch_copy_library.sh avant
 # cd /Users/dufresne/ChronoModel-SoftWare/chronomodel/QtInstaller_ChronoModel/
-# sh QtIFW_script.sh
+# sh QtIFW_script_macOS.sh
 #_____________________________________
 
 clear
 # _________________________
-echo "-  1 Script compile intaller "
+echo " 1 - Script compile intaller "
 # -------------------------------------------------------
 
 # -------------------------------------------------------
@@ -15,24 +15,24 @@ echo "-  1 Script compile intaller "
 # -------------------------------------------------------
 ROOT_PATH=$(dirname $0)
 # _________________________
-echo "-  2 copy the BUNDLE "
+echo " 2 - Copy the BUNDLE "
 # -------------------------------------------------------
 
 # make -j12 in /Users/dufresne/ChronoModel-SoftWare/build-Chronomodel-Qt_6_4_3_for_macOS-Release
 
-RELEASE_PATH=/Users/dufresne/ChronoModel-SoftWare/build-Chronomodel-Qt_6_5_2_for_macOS-Release/build/release/
+RELEASE_PATH=/Users/dufresne/ChronoModel-SoftWare/build-Chronomodel-Qt_6_5_3_for_macOS-Release/build/release/
 BUNDLE="$RELEASE_PATH"chronomodel.app
 #/Users/dufresne/ChronoModel-SoftWare/chronomodel/QtIFW_src/installer-packages/chronomodel_QtIFW.composant2/data
 cp -R $BUNDLE installer-packages/chronomodel_QtIFW.composant1/data
 
 QT_BIN_PATH=/Users/dufresne/Qt/Tools/QtInstallerFramework/4.5/bin
 
-VERSION=3.2.6
+VERSION=3.2.7
 
-echo "-  3 Execution de binarycreator"
+echo " 3 - Execution de binarycreator"
 ${QT_BIN_PATH}/binarycreator --offline-only -c installer-config/config.xml -p installer-packages ChronoModel_v${VERSION}_Installer
 
 # _________________________
-echo "-  4 view the BUNDLE : /Users/dufresne/ChronoModel-SoftWare/chronomodel/QtInstaller_ChronoModel"
+echo " 4 - view the BUNDLE : /Users/dufresne/ChronoModel-SoftWare/chronomodel/QtInstaller_ChronoModel"
 # -------------------------------------------------------
 
